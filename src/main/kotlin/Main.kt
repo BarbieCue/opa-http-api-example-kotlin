cd79e72dbdd89caaab3066de85584b2bfc490f28
René Knop <knopr@haufe.com>
2022-11-08 18:04:11 +0100

    bind decision to OPA response

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -32,28 +32,27 @@ suspend fun main() {
             authenticate("auth-basic") {
                 get("{...}") {
                     val user = call.principal<UserIdPrincipal>()?.name
-                    println("user: $user")
                     val method = call.request.httpMethod.value
-                    println("method: $method")
                     val path = call.request.path()
-                    println("path: $path")
-                    val token = call.request.queryParameters.get("token")
-                    println("token: $token")
+                    val token = call.request.queryParameters["token"]
 
-                    val authenticated = checkPolicy(user, method, path, token)
+                    val isAuthorized = checkPolicy(user, method, path, token)
 
-                    call.respondText("Hello, world!")
+                    if (isAuthorized)
+                        call.respondText("You are authorized! :)")
+                    else
+                        call.respond(HttpStatusCode.Unauthorized, "Nope!")
                 }
             }
         }
     }.start(wait = true)
 }
 
+// Input JSON for OPA request
 @Serializable
 data class OpaRequestBody(
     val input: Input
 )
-
 @Serializable
 data class Input(
     val user: String?,
@@ -62,13 +61,17 @@ data class Input(
     val token: String?,
 )
 
-suspend fun checkPolicy(user: String?, method: String, path: String, token: String?): String {
-    val opaData = OpaRequestBody(Input(user, method, path.removePrefix("/").split("/"), token))
+// OPA response JSON (stripped)
+@Serializable
+data class Result(
+    val allow: Boolean
+)
+@Serializable
+data class OpaResponse(
+    val result: Result
+)
 
-    // "/httpapi/auth_example" comes from the example-policy.rego file
-    // https://www.openpolicyagent.org/docs/latest/policy-language/
-    val opaUrl = "http://localhost:8181/v1/data/httpapi/auth_example"
-
+suspend fun checkPolicy(user: String?, method: String, path: String, token: String?): Boolean {
     val client = HttpClient(CIO) {
         install(Logging) {
             logger = Logger.DEFAULT
@@ -78,18 +81,21 @@ suspend fun checkPolicy(user: String?, method: String, path: String, token: Stri
             json(Json {
                 prettyPrint = true
                 isLenient = true
+                ignoreUnknownKeys = true
             })
         }
     }
 
-    val response = client.post(opaUrl) {
+    // "/httpapi/auth_example" comes from the example-policy.rego file
+    // https://www.openpolicyagent.org/docs/latest/policy-language/
+    val opaUrl = "http://localhost:8181/v1/data/httpapi/auth_example"
+
+    val opaData = OpaRequestBody(Input(user, method, path.removePrefix("/").split("/"), token))
+
+    val response: OpaResponse = client.post(opaUrl) {
         contentType(ContentType.Application.Json)
         setBody(opaData)
-    }
+    }.body()
 
-    if (response.status.value >= 300) {
-        throw Exception("Error checking auth! status: ${response.status} and message: ${response.bodyAsText()}")
-    }
-
-    return response.bodyAsText()
+    return response.result.allow
 }
